9f24b1f7d5fcd63dc9a8ac61ed2ca8a5c5ff9cd1
Adam Bajguz <adam.bajguz@gmail.com>
2018-12-01 20:31:22 +0100

    JS code

---
+++ src/assets/coin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="coin" tilewidth="64" tileheight="64" tilecount="4" columns="4">
+ <image source="coin.png" trans="ffffff" width="256" height="64"/>
+</tileset>
