2c3d2daa4c632d58d503e59d56c2a3f4e15809aa
Adam Bajguz <adam.bajguz@gmail.com>
2018-12-06 13:31:04 +0100

    Some refactor

--- src/js/main.js
+++ src/js/main.js
@@ -24,11 +24,9 @@ var game = new Phaser.Game(config);
 var map;
 var player;
 var cursors;
-var groundLayer, coinLayer;
+var groundLayer, decorationLayer, keyLayer, coinObjects, coinObjectsGroup;
 var text;
 var score = 0;
-var coinLayer;
-var overlapObjectsGroup;
 
 function preload() {
     // map made with Tiled in JSON format
@@ -52,46 +50,70 @@ function preload() {
     this.load.atlas('player', 'assets/player.png', 'assets/player.json');
 }
 
-function create() {
+function loadMap(prevThis) {
     // load the map 
-    map = this.make.tilemap({
+    map = prevThis.make.tilemap({
         key: 'map'
     });
 
     // tiles for the ground layer
     var groundTiles = map.addTilesetImage('cave');
+    var coinTiles = map.addTilesetImage('coin');
 
     // create the ground layer
     groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
     // the player will collide with this layer
     groundLayer.setCollisionByExclusion([-1]);
 
+    decorationLayer = map.createDynamicLayer('Decorations', groundTiles, 0, 0);
 
+    prevThis.physics.world.bounds.width = groundLayer.width;
+    prevThis.physics.world.bounds.height = groundLayer.height;
+}
 
-
-    // set the boundaries of our game world
-    this.physics.world.bounds.width = groundLayer.width;
-    this.physics.world.bounds.height = groundLayer.height;
-
-    // create the player sprite    
-    player = this.physics.add.sprite(64, 64, 'player');
+function createPlayer(prevThis) {
+    // create the player sprite
+    player = prevThis.physics.add.sprite(64, 64, 'player');
     player.setBounce(0.2); // our player will bounce from items
     player.setCollideWorldBounds(true); // don't go out of the map    
 
     // small fix to our player images, we resize the physics body object slightly
-    player.body.setSize(player.width, player.height - 8);
-
+    player.body.setSize(player.width, player.height - 9);
+    player.y = 10*64;
     // player will collide with the level tiles 
-    this.physics.add.collider(groundLayer, player);
+    prevThis.physics.add.collider(groundLayer, player);
+}
 
+function createPlayerAnims(prevThis) {
+    prevThis.anims.create({
+        key: 'walk',
+        frames: prevThis.anims.generateFrameNames('player', {
+            prefix: 'p1_walk',
+            start: 1,
+            end: 8,
+            zeroPad: 2
+        }),
+        frameRate: 8,
+        repeat: -1
+    });
 
+    prevThis.anims.create({
+        key: 'idle',
+        frames: [{
+            key: 'player',
+            frame: 'p1_stand'
+        }],
+        frameRate: 8,
+    });
+}
+
+function createCoins(prevThis) {
     // coin image used as tileset
-    var coinTiles = map.addTilesetImage('coin');
     // add coins as tiles
     //coinLayer = map.createDynamicLayer('Coins', coinTiles, 0, 0);
-    this.anims.create({
+    prevThis.anims.create({
         key: 'spin',
-        frames: this.anims.generateFrameNumbers('coin', {
+        frames: prevThis.anims.generateFrameNumbers('coin', {
             start: 0,
             end: 3
         }),
@@ -103,68 +125,60 @@ function create() {
     coinLayer = map.createFromObjects('CoinsObj', 101, {
         key: 'coin'
     });
-    
-    // this.anims.play('spin', coinLayer);
-    this.coinGroup = this.physics.add.staticGroup();
 
-    overlapObjectsGroup = this.physics.add.staticGroup({
-
-    });
-
+    coinObjectsGroup = prevThis.physics.add.staticGroup({});
 
     coinLayer.forEach(coin => {
-        let obj = overlapObjectsGroup.create(coin.x, coin.y, 'coin');
+        let obj = coinObjectsGroup.create(coin.x, coin.y, 'coin');
         obj.body.width = coin.width;
         obj.body.height = coin.height;
         obj.visible = true;
         coin.visible = false;
-        this.anims.play('spin', obj);
+        prevThis.anims.play('spin', obj);
 
     });
-    overlapObjectsGroup.refresh(); //physics body needs to refresh
-    console.log(overlapObjectsGroup);
+    coinObjectsGroup.refresh(); //physics body needs to refresh
+    console.log(coinObjectsGroup);
 
-    this.physics.add.overlap(player, overlapObjectsGroup, collectCoin, null, this);
+    prevThis.physics.add.overlap(player, coinObjectsGroup, collectCoin, null, prevThis);
 
-    this.anims.play('spin', coinLayer);
+}
 
-
-    var keyTiles = map.addTilesetImage('key');
+function createKeys(prevThis) {
+    keyTiles = map.addTilesetImage('key');
     // add coins as tiles
     keyLayer = map.createDynamicLayer('Keys', keyTiles, 0, 0);
-    decorationLayer = map.createDynamicLayer('Decorations', groundTiles, 0, 0);
 
     // coinLayer.setTileIndexCallback(101, collectCoin, this);
     // when the player overlaps with a tile with index 101, collectCoin 
-    // will be called    
+    // will be called
     // this.physics.add.overlap(player, coinLayer);
 
-    keyLayer.setTileIndexCallback(105, collectKey, this);
+    keyLayer.setTileIndexCallback(105, collectKey, prevThis);
     // when the player overlaps with a tile with index 101, collectCoin 
     // will be called    
-    this.physics.add.overlap(player, keyLayer);
+    prevThis.physics.add.overlap(player, keyLayer);
+}
 
-    // player walk animation
-    this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNames('player', {
-            prefix: 'p1_walk',
-            start: 1,
-            end: 8,
-            zeroPad: 2
-        }),
-        frameRate: 8,
-        repeat: -1
+function createScoreText(prevThis) {
+    // this text will show the score
+    text = prevThis.add.text(20, 20, 'Score: 0', {
+        fontSize: '20px',
+        fill: '#ffffff'
     });
-    // idle with only one frame, so repeat is not neaded
-    this.anims.create({
-        key: 'idle',
-        frames: [{
-            key: 'player',
-            frame: 'p1_stand'
-        }],
-        frameRate: 8,
-    });
+    
+    // fix the text to the camera
+    text.setScrollFactor(0);
+}
+
+function create() {
+    loadMap(this);
+    createPlayer(this);
+    createPlayerAnims(this);
+    createCoins(this);
+    createKeys(this);
+    createScoreText(this);
+
 
 
     cursors = this.input.keyboard.createCursorKeys();
@@ -177,13 +191,7 @@ function create() {
     // set background color, so the sky is not black    
     this.cameras.main.setBackgroundColor('#140C1C');
 
-    // this text will show the score
-    text = this.add.text(20, 570, '0', {
-        fontSize: '20px',
-        fill: '#ffffff'
-    });
-    // fix the text to the camera
-    text.setScrollFactor(0);
+
 }
 
 // this function will be called when the player touches a coin
@@ -193,16 +201,21 @@ function collectCoin(player, coin) {
 
     coin.destroy();
 
-    score++; // add 10 points to the score
-    text.setText(score); // set the text to show the current score
+    score++;
+    updateScore();
 }
 
 // this function will be called when the player touches a coin
 function collectKey(sprite, tile) {
     keyLayer.removeTileAt(tile.x, tile.y); // remove the tile/coin
-    score++; // add 10 points to the score
-    text.setText(score); // set the text to show the current score
+    score++;
+    updateScore();
     return false;
+}
+
+function updateScore()
+{
+    text.setText('Score: ' + score); // set the text to show the current score
 }
 
 
@@ -211,15 +224,17 @@ function update(time, delta) {
         player.body.setVelocityX(-200);
         player.anims.play('walk', true); // walk left
         player.flipX = true; // flip the sprite to the left
+
     } else if (cursors.right.isDown) {
         player.body.setVelocityX(200);
         player.anims.play('walk', true);
         player.flipX = false; // use the original sprite looking to the right
+
     } else {
         player.body.setVelocityX(0);
         player.anims.play('idle', true);
     }
-    // jump 
+
     if (cursors.up.isDown && player.body.onFloor()) {
         player.body.setVelocityY(-700);
     }
